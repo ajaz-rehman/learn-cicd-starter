e36ffe6fe70367eda572ea7dc30b7072f24f1f00
Ajaz Rehman <ajazurrehman4@gmail.com>
2024-11-22 09:31:07 +0000

    Fix test

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -30,7 +30,7 @@ func TestGetAPIKey(t *testing.T) {
 		{
 			name: "Valid Authorization Header",
 			headers: http.Header{
-				"Authorization": []string{"ApiKey somekeys"},
+				"Authorization": []string{"ApiKey somekey"},
 			},
 			expectedKey:   "somekey",
 			expectedError: nil,
